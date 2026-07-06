6e33b8963cbfea0fdaeb184f99c6f73155d4faf2
Danylo Kykosh <dkykosh@novomind.com>
2026-07-06 15:30:27 +0200

    added the logo for the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,28 @@
 <template>
-  <div class="ingredient-list">
-    <Button @click="fetchData">Zutaten ohne rezpet</Button>
-    <p v-if="loading">Lade Zutaten...</p>
-    <p v-else-if="error">{{ error }}</p>
-
-    <div v-else class="ingredient-grid">
-      <IngredientCard
-        v-for="ingredient in ingredients"
-        :key="ingredient.id"
-        :ingredient="ingredient"
-      />
+    <div class="home-page">
+        <img src="../assets/kraut-und-rueben.png" alt="Kraut und Rüben" class="home-logo" />
     </div>
-  </div>
 </template>
 
 <script>
-import api from '../services/api';
-import IngredientCard from '../components/IngredientCard.vue';
-
 export default {
-    name: 'Home',
-    components: {
-        IngredientCard
-    },
-    data() {
-        return {
-            ingredients: [],
-            loading: false,
-            error: null
-        };
-    },
-    methods: {
-        async fetchData(){
-            try {
-                this.loading = true;
-                const response = await api.get('/zutat/ohne-rezept');
-                this.ingredients = response.data;
-                this.error = null;
-            } catch (err) {
-                this.error = 'Failed to fetch data. Check console for details';
-                console.error(err);
-            }  finally {
-                this.loading = false
-            }
-        }
-    }
-}
+    name: 'home'
+};
 </script>
 
 <style scoped>
-.ingredient-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  gap: 1rem;
-  padding: 1rem;
+.home-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: calc(100vh - 4rem);
+    background-color: #1d1e21;
+    padding: 2rem;
+}
+
+.home-logo {
+    max-width: 480px;
+    width: 100%;
+    height: auto;
 }
 </style>
